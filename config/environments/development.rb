4e1ebf4f942644127d48ea40e9556477426c2651
Domhnall <domhnallmurphy@hotmail.com>
2022-11-27 21:18:50 +0000

    Extend `config.hosts` on dev to support ngrok and custom subdomain

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,4 +67,8 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # Permit SSL requests in development
+  config.hosts << "dev.vector-logic.com"
+  config.hosts << ".eu.ngrok.io"
 end
